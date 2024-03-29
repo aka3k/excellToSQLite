cd8afef2c9f3aedec6f01c283a2e303e115fb35a
unknown <csrndr03a07h264n@iisbadoni.edu.it>
2024-03-29 18:30:21 +0100

    codice a caso

--- excellToSQLite.py
+++ excellToSQLite.py
@@ -1,30 +1,44 @@
-import random as rnd
-import math
+import pandas as pd
 
-def is_prime(num):
-    if num <= 1:
-        return False
-    if num <= 3:
-        return True
-    if num % 2 == 0 or num % 3 == 0:
-        return False
+def read_excel_file(file_name):
+    # Load the Excel file into a pandas DataFrame
+    df = pd.read_excel(file_name, header=None, skiprows=3)
 
-    for i in range(5, int(math.sqrt(num)) + 1, 6):
-        if num % i == 0 or num % (i + 2) == 0:
-            return False
-    return True
+    # Define the required information
+    year = df.iloc[2, 1]
+    cls = df.loc[1, 1]
+    period = df.loc[2, 2]
 
-def generate_random_prime_numbers(n, max_iterations=1000):
-    prime_numbers = []
-    attempts = 0
+    # Define the number of rows to skip between students
+    rows_to_skip = 18
 
-    while len(prime_numbers) < n and attempts < max_iterations:
-        num = rnd.randint(1, 10000)
-        if is_prime(num):
-            prime_numbers.append(num)
-        attempts += 1
+    # Create an empty list to store student data
+    student_data = []
 
-    return prime_numbers
+    # Iterate through each student and append their data to the list
+    for i in range(0, df.shape[0], rows_to_skip):
+        student_name = df.loc[i, 3]
+        student_marks = df.iloc[i: i + rows_to_skip - 1, 4].tolist()
 
-n = 5
-print(generate_random_prime_numbers(n))
+        # Calculate the average mark
+        student_average = sum(float(mark) for mark in student_marks if mark != 'X') / (len(student_marks) - 1)
+
+        student_data.append([student_name, *student_marks, student_average])
+
+    return year, cls, period, student_data
+
+def print_student_data(year, cls, period, student_data):
+    print(f'Year: {year}')
+    print(f'Class: {cls}')
+    print(f'Period: {period}')
+
+    print("\nStudent Data:")
+    for data in student_data:
+        print(data)
+
+# Get the filename from user input
+file_name = "1Sin.xls"
+
+# Read the Excel file and print the student data
+year, cls, period, student_data = read_excel_file(file_name)
+print_student_data(year, cls, period, student_data)
