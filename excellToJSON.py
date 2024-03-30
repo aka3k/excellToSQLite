3d80da7d92d98a6b5bf55449f26fd8dfa4a2d8b0
aka3k <acasari2003@gmail.com>
2024-03-30 15:53:09 +0100

    fixati alcuni problemi relativi all inserimento di righe con tutti i valori null nel json

--- excellToJSON.py
+++ excellToJSON.py
@@ -1,23 +1,29 @@
 import pandas as pd
-#installare pandas e xlrd
+
+# Installare pandas e xlrd se necessario
 
 # Carica il file Excel in un DataFrame
 path_excel = '1Sin.xls'
 df = pd.read_excel(path_excel, engine='xlrd', usecols=['Pr.', 'Alunno', 'RELIGIONE', 
-                'LINGUA E LETT.IT', 'LINGUA INGLESE', 'STORIA', 'EDUCAZIONE CIVICA',
-                'MATEMATICA', 'DIRITTO ED ECONOMIA', 'FISICA', 'CHIMICA', 'Tecn.informatiche',
-                'Tecn.e Tecn.di rappr','SC.DELLA TERRA/GEO', 'SCIENZE MOT. E SPORT', 'COMPORTAMENTO', 
-                'Media', 'Esito'], skiprows=3)# Select columns 1 to 18 (0-based indexing)
+                    'LINGUA E LETT.IT', 'LINGUA INGLESE', 'STORIA', 'EDUCAZIONE CIVICA',
+                    'MATEMATICA', 'DIRITTO ED ECONOMIA', 'FISICA', 'CHIMICA', 'Tecn.informatiche',
+                    'Tecn.e Tecn.di rappr','SC.DELLA TERRA/GEO', 'SCIENZE MOT. E SPORT', 'COMPORTAMENTO', 
+                    'Media', 'Esito'], skiprows=3)# Seleziona le colonne che hanno questo nome
 
+# Trova l'indice della prima riga con tutti i valori nulli
+for i, row in df.iterrows():
+    if all(pd.isnull(row)):
+        break
 
+# Crea un nuovo DataFrame con solo le righe fino all'indice trovato
+df_filtered = df.iloc[:i]
 
 # Converti il DataFrame in JSON
-json_data = df.to_json(orient='records', indent=4)
+json_data = df_filtered.to_json(orient='records', indent=4)
 
 # Scrivi i dati JSON su un file
 path_json = 'final_result.json'
 with open(path_json, 'w') as json_file:
     json_file.write(json_data)
-
 
 print("Conversione completata con successo.")
