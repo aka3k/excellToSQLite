6ca42bca543295ff31a05d890061b353b3632836
ClaudioerBoncio <bnccld05r03c933d@iisbadoni.edu.it>
2024-03-30 15:47:37 +0100

    codice funzionante excel to jason: problema risolto non prendendo un range ma selezionando le colonne tramite nome

--- excellToJSON.py
+++ excellToJSON.py
@@ -1,37 +1,23 @@
-import json
 import pandas as pd
+#installare pandas e xlrd
 
 # Carica il file Excel in un DataFrame
 path_excel = '1Sin.xls'
-df = pd.read_excel(path_excel, engine='xlrd', usecols=range(0, 28), skiprows=3) # Select columns 1 to 18 (0-based indexing)
-
-# Rinomina le colonne
-df.columns = ['Pr.', 'Alunno', 'spazioVuoto1', 'spazioVuoto2', 'spazioVuoto3', 'spazioVuoto4',
-                'spazioVuoto5', 'spazioVuoto6', 'spazioVuoto7', 'spazioVuoto8','RELIGIONE', 
+df = pd.read_excel(path_excel, engine='xlrd', usecols=['Pr.', 'Alunno', 'RELIGIONE', 
                 'LINGUA E LETT.IT', 'LINGUA INGLESE', 'STORIA', 'EDUCAZIONE CIVICA',
                 'MATEMATICA', 'DIRITTO ED ECONOMIA', 'FISICA', 'CHIMICA', 'Tecn.informatiche',
                 'Tecn.e Tecn.di rappr','SC.DELLA TERRA/GEO', 'SCIENZE MOT. E SPORT', 'COMPORTAMENTO', 
-                'Media', 'spazioVuoto9', 'spazioVuoto10', 'Esito']
+                'Media', 'Esito'], skiprows=3)# Select columns 1 to 18 (0-based indexing)
 
-# Crea una lista per memorizzare i dizionari
-json_data = []
 
-# Itera su ogni riga del DataFrame
-for row in df.itertuples():
-    # Crea un dizionario per ogni riga
-    record = {}
-    
-    # Aggiungi solo le colonne con valori non nulli
-    for col, value in zip(df.columns, row):
-        if not pd.isnull(value):
-            record[col] = value
-    
-    # Aggiungi il dizionario alla lista
-    json_data.append(record)
+
+# Converti il DataFrame in JSON
+json_data = df.to_json(orient='records', indent=4)
 
 # Scrivi i dati JSON su un file
 path_json = 'final_result.json'
 with open(path_json, 'w') as json_file:
-    json.dump(json_data, json_file, indent=4)
+    json_file.write(json_data)
+
 
 print("Conversione completata con successo.")
